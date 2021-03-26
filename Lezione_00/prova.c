5e239a9ee993d765a67859c2197169d7a3ef56a1
arkkimede <arkkimede@gmail.com>
2021-03-26 22:54:44 +0100

    Aggiunto sorgenti Lezione 0

---
+++ Lezione_00/prova.c
@@ -0,0 +1,10 @@
+/* Programma prova.c per  
+   verificare che lo script di compilazione 
+   funziona correttamente */
+
+#include <stdio.h>
+int main()
+{
+    printf("Tutto funziona correttamente\n");  
+    return 0;
+}
